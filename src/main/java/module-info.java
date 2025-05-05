c2d33bcfa6fe95f6ca8b06697646544f01c6d8e2
KaykySiq <josekayky@hotmail.com>
2025-05-05 00:37:36 -0300

    Feix: Ajusta dependencias e modulos.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,9 @@ module com.ibametro.folhaponto.folhaponto {
     requires jakarta.annotation;
     requires jakarta.persistence;
     requires org.hibernate.orm.core;
+    requires org.apache.poi.poi;
+    requires java.desktop;
+    requires org.json;
 
 
     opens com.ibametro.folhaponto.folhaponto to javafx.fxml;
@@ -11,8 +14,9 @@ module com.ibametro.folhaponto.folhaponto {
     exports com.ibametro.folhaponto.folhaponto.controller;
     opens com.ibametro.folhaponto.folhaponto.controller to javafx.fxml;
 
-    opens com.ibametro.folhaponto.folhaponto.domain to org.hibernate.orm.core, jakarta.persistence;
-    exports com.ibametro.folhaponto.folhaponto.service;
-    opens com.ibametro.folhaponto.folhaponto.service to javafx.fxml;
+    opens com.ibametro.folhaponto.folhaponto.domain to org.hibernate.orm.core, jakarta.persistence, javafx.base;
+    exports com.ibametro.folhaponto.folhaponto.services;
+    opens com.ibametro.folhaponto.folhaponto.services to javafx.fxml;
+
 
 }
